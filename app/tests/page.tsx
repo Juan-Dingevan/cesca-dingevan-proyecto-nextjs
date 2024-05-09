729f351029e033dd58c425d281c47ef24a43cfff
Juan Dingevan <jidingevan@gmail.com>
2024-05-09 18:01:07 -0300

    Testing db integration.

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { sql } from '@vercel/postgres';
 
-const TestsPage = () => {
-  return (
-    <div>Tests!</div>
-  )
+const TestsPage = async () => {
+    let data = await sql`SELECT * FROM pg_catalog.pg_tables;`;
+
+    return (
+        <div>
+            <p>Tests</p>
+            <ul>
+                {data.rows.map((row, index) => (
+                    <li key={index}>{JSON.stringify(row)}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default TestsPage
