e149659205e8dc898da986ee56d74a307ca623d0
Martina Cesca <marticesca25@gmail.com>
2024-05-22 11:30:37 -0300

    Create product form done

--- app/components/abm/CreateForm.tsx
+++ app/components/abm/CreateForm.tsx
@@ -4,49 +4,87 @@ import Link from 'next/link';
 
 import { useFormState } from 'react-dom';
 import { Button } from './CreateButton';
+import { createProduct } from '@/app/lib/actions';
 
 //FORMULARIO: id: string; name: string; description: string; price: number; category: string; vegan: boolean; gluten_free: boolean; date_added: Date; img_link: string;
 
 
 export default function Form() {
-  //const initialState = { message: null, errors: {} };
-  //const [state, dispatch] = useFormState(createInvoice, initialState);
+  const initialState = {   message:"", errors: {} };
+  const [state, dispatch] = useFormState(createProduct, initialState);
   return (
-    //<form action={dispatch}>
-    <form>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
       <div className="input-data">
-        <input type="text" placeholder="Nombre del producto" className="input-text"/>
+        <input 
+        id="name"
+        name="name"
+        type="text" 
+        placeholder="Nombre del producto" 
+        className="input-text"/>
         <div className="underline"></div>
       </div>
       <div className="input-data textarea">
-        <textarea rows={8} cols={80} placeholder="Descripción del producto" className="input-textarea"></textarea>
+        <textarea 
+         id="description"
+         name="description"
+         rows={8} 
+         cols={80} 
+         placeholder="Descripción del producto" 
+         className="input-textarea"></textarea>
         <div className="underline"></div>
       </div>
       <div className="input-data number">
-        <input type="number" min={0} placeholder="Precio del producto" className="input-number"/>
+        <input 
+        id="price"
+        name="price"
+        type="number" 
+        step="0.1"
+        min={0} 
+        placeholder="Precio del producto" 
+        className="input-number"/>
         <div className="underline"></div>
       </div>
       <div className="input-data">
-        <input type="text" placeholder="Categoria del producto" className="input-text"/>
+        <input 
+        id="category"
+        name="category"
+        type="text" 
+        placeholder="Categoria del producto" 
+        className="input-text"/>
         <div className="underline"></div>
       </div>
       <div className="input-data boolean flex items-center">
-        <input type="checkbox" className="input-boolean mr-2" />
+        <input 
+        id="vegan"
+        name="vegan"
+        type="checkbox" 
+        className="input-boolean mr-2" />
         <span>Vegan</span>
         <div className="underline"></div>
       </div>
       <div className="input-data boolean flex items-center">
-        <input type="checkbox" className="input-boolean mr-2" />
+        <input 
+        id="gluten_free" 
+        name="gluten_free" 
+        type="checkbox" 
+        className="input-boolean mr-2" />
         <span>Gluten Free</span>
         <div className="underline"></div>
       </div>
       <div className="input-data">
-        <input type="url" placeholder="Link de la imagen" className="input-text w-full border-b-2 border-gray-300 focus:border-blue-500 focus:outline-none py-2" />
+        <input 
+        id="img_link"
+        name="img_link"
+        type="url" 
+        placeholder="Link de la imagen" 
+        className="input-text w-full border-b-2 border-gray-300 focus:border-blue-500 focus:outline-none py-2" />
         <div className="underline"></div>
       </div>
 
       </div>
+      
+       
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/admin"
---
+++ app/lib/actions.ts
@@ -0,0 +1,89 @@
+'use server';
+
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+
+//observar tipos de datos (gluten_free, vegan)
+const FormSchema = z.object({
+  id: z.string(),
+  name: z.string({
+    invalid_type_error: 'Please enter a name.',
+  }),
+  description: z.string(),
+  price: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  category: z.string({
+    invalid_type_error: 'Please select a category.',}),
+  vegan: z.boolean(),
+  gluten_free: z.boolean(),
+  //date_added: z.Date(),
+  date_added: z.string(),
+  img_link: z.string(),
+});
+ 
+//const CreateProduct = FormSchema.omit({ id: true, date_added: true });
+const CreateProduct = FormSchema.omit({ id: true, date_added: true });
+
+export type State = {
+    errors?: {
+      //name?: string[];
+      //description?: string[];
+      //price?: string[];
+      //category?: string[];
+      //vegan?: string[];
+      //gluten_free?: string[];
+      //img_link?: string[];
+    };
+    message?: string | null;
+  };
+
+  //export async function createInvoice(prevState: State, formData: FormData) {
+    export async function createProduct(prevState: State, formData: FormData) {
+    // Validate form using Zod
+    const validatedFields = CreateProduct.safeParse({
+      name: formData.get('name'),
+      description: formData.get('description'),
+      price: formData.get('price'),
+      category: formData.get('category'),
+      vegan: Boolean(formData.get('vegan')),
+      gluten_free: Boolean(formData.get('gluten_free')),
+      img_link: formData.get('img_link'),
+    });
+    
+   
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Product.',
+      };
+    }
+   
+    // Prepare data for insertion into the database
+    const { name, description, price, category, vegan, gluten_free, img_link } = validatedFields.data;
+    const priceInCents = price * 100;
+    const date = new Date().toISOString().split('T')[0];
+   
+    // Insert data into the database
+    try {
+    await sql`
+        INSERT INTO ventanita.products (name, description, price, category, vegan, gluten_free, date_added, img_link)
+        VALUES (${name}, ${description}, ${priceInCents}, ${category}, ${vegan}, ${gluten_free}, ${date}, ${img_link})
+      `;
+    } catch (error) {
+      // If a database error occurs, return a more specific error.
+      return {
+        message: 'Database Error: Failed to Create Invoice.',
+      };
+    }
+    console.log("here4");
+   
+    // Revalidate the cache for the invoices page and redirect the user.
+    revalidatePath('/admin');
+    redirect('/admin');
+  }
