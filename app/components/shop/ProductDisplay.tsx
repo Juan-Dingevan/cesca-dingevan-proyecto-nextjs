9ff721199e9eb88340b27a1ef5bd8b03f79356e9
Juan Dingevan <jidingevan@gmail.com>
2024-05-27 15:09:37 -0300

    Display helpful text when there are no results found in shop.

--- app/components/shop/ProductDisplay.tsx
+++ app/components/shop/ProductDisplay.tsx
@@ -2,6 +2,7 @@ import { unstable_noStore as noStore } from 'next/cache';
 import ProductCard from './ProductCard';
 import { fetchFilteredProducts, numberOfPagesNeededForProducts } from '@/app/lib/data';
 import Pagination from './Pagination';
+import NoResults from './NoResults';
 
 export default async function ProductDisplay({
     query,
@@ -20,16 +21,19 @@ export default async function ProductDisplay({
     const products = await fetchFilteredProducts(query, categories, currentPage)
     const totalPages = await numberOfPagesNeededForProducts(query, categories)
 
+    const atLeastOneProduct = products.length > 0
+
     return (
         <div className='py-4 flex flex-col justify-between h-screen'>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-10 p-10">
+            {atLeastOneProduct && <div className="grid grid-cols-1 md:grid-cols-3 gap-10 p-10">
                 {products.map(
                     (product) => <ProductCard key={product.id} product={product} />
                 )}
-            </div>
-            <div className="mt-3 flex w-full justify-center">
+            </div>}
+            {atLeastOneProduct && <div className="mt-3 flex w-full justify-center">
                 <Pagination totalPages={totalPages} />
-            </div>
+            </div>}
+            {!atLeastOneProduct && <NoResults />}
         </div>
     )
 }
